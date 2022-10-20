405b9b6597c9f8996b87390edcd888fcf3c2fd98
Xiknu Nighttail <xiknu@fuzzball.dev>
2022-10-20 08:57:54 -0400

    Revert everything back to static HTML.
    This reverts commit 6357d3d43df9169b5f3f08d18bc4cd4155001611.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Save the foxes!' });
-});
-
-module.exports = router;
